76b448b115c7e57bb46b5d357181794c7bb81415
Harini <harinee2015@gmail.com>
2017-07-01 12:40:10 +0530

    Create Height.java

---
+++ Height.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Height
+{
+  public static void main(String args[])throws IOException
+  {
+  Bufferedredaer br=newBufferedReader(new InputStreamReader(System.in));
+  String a=br.readLine();
+  
